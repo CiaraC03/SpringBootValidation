262b23d360b5d3b350ca7523e174c9e76b170dba
G00402357@atu.ie <g00402357@atu.ie>
2023-11-15 14:36:52 +0000

    Created class

---
+++ src/main/java/ie/atu/springbootvalidation/Person.java
@@ -0,0 +1,27 @@
+package ie.atu.springbootvalidation;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.Email;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class Person {
+    @NotBlank(message="Name cannot be blank")
+    @Size(min =2, max = 50, message = "Name must be between 2 and 50 characters")
+    private String name;
+
+    @NotBlank(message="EmployeeId cannot be blank")
+    @Size(min =3, max = 10, message ="EmployeeId must be between 2 and 10 numbers")
+    private String employeeId;
+
+    @Email(message = "Invalid email address")
+    private String email;
+
+
+
+
+
+}
